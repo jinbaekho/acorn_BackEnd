00a5aeee7c406a708d04b28c191704806fee6eee
hbi <hbi@DESKTOP-Q60681L>
2023-08-09 09:13:49 +0900

    0809

--- src/com/acorn/d0804/Study1.java
+++ src/com/acorn/d0804/Study1.java
@@ -3,12 +3,12 @@ package com.acorn.d0804;
 import java.util.Scanner;
 
 public class Study1 {
+	static int[] game = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	public static void main(String[] args) {
-		int[] game = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		newN(game);		
+		newN();		
 	}
 	
-	public static void newN(int[] game) {
+	public static void newN() {
 		while (true) {
 			int x = (int) Math.round(Math.random() * 8);
 			if (game[x] == 0) {
@@ -16,17 +16,39 @@ public class Study1 {
 				break;
 			}
 		}
-		DP(game);
+		DP();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("이동");
 		int MV = sc.nextInt();
 		if(MV==5) {newN(game);}
+		else if(MV==2) {DOWN(game);}
 //		sc.close();
 	}
 	
-	public static void DP(int[] game) {
+	public static void DP() {
 		System.out.println(game[0] + " " + game[1] + " " + game[2]);
 		System.out.println(game[3] + " " + game[4] + " " + game[5]);
 		System.out.println(game[6] + " " + game[7] + " " + game[8]);
 	}
+	
+	public static void DOWN() {
+		for(int i=5;i>=0;i--) {
+
+			if(game[i]==0) {continue;}
+			else {
+			if(game[i+3]==0)
+				{
+				game[i+3]=game[i]+game[i+3];
+				game[i]=0;
+				}
+			else if(game[i]==game[i+3])
+				{
+				game[i+3]=game[i]+game[i+3];
+				game[i]=0;
+				}
+			}
+		}
+		newN();
+	}
+	
 }
---
+++ src/com/acorn/d0804/test.java
@@ -0,0 +1,18 @@
+package com.acorn.d0804;
+
+public class test {
+	static int i = 123;
+	
+	
+	public static void main(String[] args) {
+		System.out.println(i);
+		cc();
+	}
+	
+	public static void cc() {
+		System.out.println(i);
+	}
+	
+	
+	
+}
